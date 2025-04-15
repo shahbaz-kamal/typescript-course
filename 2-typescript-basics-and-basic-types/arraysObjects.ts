c30fd5010920c60d1a32872692decde4703690eb
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-15 23:55:00 +0600

    watched 2-11 learned about generic types

--- 2-typescript-basics-and-basic-types/arraysObjects.ts
+++ 2-typescript-basics-and-basic-types/arraysObjects.ts
@@ -1,6 +1,6 @@
 let hobbies = ["playing", "reading"];
 
-let users: (string | number)[];
-
+// let users: (string | number)[];
+let users: Array<string | number>;
 users = [1, "max"];
 users = ["3", "5"];
