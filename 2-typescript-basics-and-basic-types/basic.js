6ee68302a224ac5ce290ebc38bcda1029a1d903e
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-15 23:30:51 +0600

    added typescript to function

---
+++ 2-typescript-basics-and-basic-types/basic.js
@@ -0,0 +1,9 @@
+var userName;
+// ...
+userName = "Max";
+// using in function
+function add(a, b) {
+    return a + b;
+}
+var summation = add(5, 7);
+console.log(summation);
--- 2-typescript-basics-and-basic-types/basic.ts
+++ 2-typescript-basics-and-basic-types/basic.ts
@@ -2,3 +2,10 @@ let userName: string ;
 
 // ...
 userName = "Max";
+
+// using in function
+function add(a:number,b:number){
+return a+b
+}
+const summation=add(5,7)
+console.log(summation)
