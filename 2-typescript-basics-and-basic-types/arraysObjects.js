69c3025b8ddc94bfa86cf230fadb3666691a7e16
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-20 22:12:55 +0600

    watched video 2-11, understood tuple types

---
+++ 2-typescript-basics-and-basic-types/arraysObjects.js
@@ -0,0 +1,8 @@
+var hobbies = ["playing", "reading"];
+// let users: (string | number)[];
+var users;
+users = [1, "max"];
+users = ["3", "5"];
+var possibleUser;
+possibleUser = ["Tamim", 25];
+console.log(possibleUser);
--- 2-typescript-basics-and-basic-types/arraysObjects.ts
+++ 2-typescript-basics-and-basic-types/arraysObjects.ts
@@ -4,3 +4,7 @@ let hobbies = ["playing", "reading"];
 let users: Array<string | number>;
 users = [1, "max"];
 users = ["3", "5"];
+let possibleUser:[string,number];
+possibleUser=["Tamim",25]
+console.log(possibleUser)
+
